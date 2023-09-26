1f1eeba3135df94a7cbf7514780b7c9091262e9e
Jayden Craig <jayden.t.craig@gmail.com>
2023-09-26 10:33:33 -0700

    getting audio

--- code.js
+++ code.js
@@ -1,1 +1,21 @@
+const AudioContext = window.AudioContext || window.webkitAudioContext;
+const audioContext = new AudioContext();
+var analyser = audioContext.createAnalyser();
+var source;
 
+//from API site
+function getLocalStream() {
+  navigator.mediaDevices
+    .getUserMedia({ audio: true })
+    .then((stream) => {
+      window.localStream = stream; // A
+      window.localAudio.srcObject = stream; // B
+      window.localAudio.autoplay = true; // C
+
+      source = audioContext.createMediaStreamSource(stream);
+    })
+    .catch((err) => {
+      console.error(`mic access needed: ${err}`);
+    });
+}
+getLocalStream();
