d1187597c04e506837f40124cb8a416abb713ffa
Jayden Craig <jayden.t.craig@gmail.com>
2023-09-28 13:37:06 -0700

    plays 110 hz on button click

--- code.js
+++ code.js
@@ -1,9 +1,11 @@
 var audioContext;
+var hz = document.getElementById("hz");
+var startBtn = document.getElementById("btn");
 var constraints = { audio: true };
 
 navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
   try {
-    audioContext = new AudioContext();
+    audioContext = new window.AudioContext();
   } catch (e) {
     alert("Web Audio API is not supported in this browser");
   }
@@ -11,9 +13,17 @@ navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
 
 //print HZ to hz span
 function changeHz() {
-  document.getElementById("hz").textContent = "Hz here";
+  hz.textContent = "Hz here";
 }
 
-document.getElementById("btn").addEventListener("click", () => {
+startBtn.addEventListener("click", () => {
   changeHz();
 });
+
+startBtn.onclick = function () {
+  oscillator = audioContext.createOscillator(); //creates oscillator
+  oscillator.type = "sine"; //chooses the type of wave
+  oscillator.frequency.value = 110; //assigning the value of oscPitch to the oscillators frequency value
+  oscillator.connect(audioContext.destination); //sends to output
+  oscillator.start(audioContext.currentTime); //starts the sound at the current time
+};
